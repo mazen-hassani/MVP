a85cab64a489160aa5682e33862a63b9af794baa
Mazen Hassani <hassani.mazen@gmail.com>
2022-01-16 21:00:11 +0100

    make new layouts

---
+++ resources/views/layouts/test-layout.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1> {{$title}} </h1>
+    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
+</div>
