20e9bb29410c3bf6a47a5868ae50456dd2b5af23
Mazen Hassani <hassani.mazen@gmail.com>
2022-01-23 20:52:43 +0100

    Revert "Revert "create new images table""
    This reverts commit 4600a3da020e8e7c0e8484047b5b18dc3258aef9.

---
+++ database/migrations/2022_01_23_194500_create_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('org_path');
+            $table->string('thumbnail_path')->nullable();
+            $table->text('description')->nullable();
+            $table->integer('votes');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')->on('users')->references('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
