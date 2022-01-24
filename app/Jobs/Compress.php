6d0b5104cf9cb7c2114c453b83d599d595d51ef2
Mazen Hassani <hassani.mazen@gmail.com>
2022-01-24 07:32:47 +0100

    refactor compress job

--- app/Jobs/Compress.php
+++ app/Jobs/Compress.php
@@ -16,26 +16,16 @@ use Intervention\Image\ImageManagerStatic;
 class Compress implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
     private $org_image_info;
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
+
     public function __construct(Image $img)
     {
-        //
         $this->org_image_info = $img;
     }
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
     public function handle()
     {
-        //
         $image = $this->org_image_info;
         $compressed_image = ImageManagerStatic::make(Storage::disk('public')->get($image->org_path))->resize(300, null, function ($constraint) {
             $constraint->aspectRatio();
@@ -45,6 +35,5 @@ class Compress implements ShouldQueue
         Storage::disk('public')->put($path, $compressed_image);
         $image->thumbnail_path = $path;
         $image->save();
-//        dd($image);
     }
 }
