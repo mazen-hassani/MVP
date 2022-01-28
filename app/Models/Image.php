3a445875cfc2beecd45832a17cd7dc94cb7179b6
Mazen Hassani <hassani.mazen@gmail.com>
2022-01-28 03:27:32 +0100

    update image and user model with the new voting relation

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -1,12 +1,29 @@
 <?php
 
 namespace App\Models;
+use App\Models\User;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    protected $fillable = ['org_path', 'thumbnail_path', 'votes', 'user_id', 'description'];
+    protected $fillable = ['org_path', 'thumbnail_path', 'user_id', 'description'];
     use HasFactory;
+
+    public function user (){
+        return $this->belongsTo(USER::class);
+    }
+
+    public function votes(){
+        return $this->belongsToMany(User::class, 'votes');
+    }
+
+    public function countUpVotes(){
+        return $this->votes()->where('value', '=', '1')->count();
+    }
+
+    public function countDownVotes(){
+        return $this->votes()->where('value', '=', '-1')->count();
+    }
 }
