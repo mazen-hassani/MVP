dab050646edac4512a185ffa9d5d0588053b50a4
Mazen Hassani <hassani.mazen@gmail.com>
2022-01-28 03:27:48 +0100

    update compress images job

--- app/Jobs/Compress.php
+++ app/Jobs/Compress.php
@@ -10,6 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
+use Intervention\Image\ImageManager;
 use Intervention\Image\ImageManagerStatic;
 
 class Compress implements ShouldQueue
@@ -29,9 +30,14 @@ class Compress implements ShouldQueue
     public function handle()
     {
         $image = $this->org_image_info;
-        $compressed_image = ImageManagerStatic::make(Storage::disk('public')->get($image->org_path))->resize(300, null, function ($constraint) {
+
+        $compressed_image = ImageManagerStatic::make(Storage::disk('public')->get($image->org_path));
+        $crop_width = ($compressed_image->width() > $compressed_image->height()) ? $compressed_image->height() : $compressed_image->width();
+        $compressed_image->crop($crop_width, $crop_width )->resize(300, 300, function ($constraint) {
             $constraint->aspectRatio();
         })->encode('jpg', 100);
+
+        $compressed_image= (new \Intervention\Image\ImageManager)->make($compressed_image)->colorize(-100, 0, 100);;
 
         $path = '/compressed/'.Str::uuid().'.jpg';
         Storage::disk('public')->put($path, $compressed_image);
