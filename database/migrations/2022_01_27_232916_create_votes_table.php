dddb38dd7e13924071bf23cb4b8baf6b1f0c4634
Mazen Hassani <hassani.mazen@gmail.com>
2022-01-28 03:26:45 +0100

    update images table and votes table

--- database/migrations/2022_01_27_232916_create_votes_table.php
+++ database/migrations/2022_01_27_232916_create_votes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateImagesTable extends Migration
+class CreateVotesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateImagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('images', function (Blueprint $table) {
+        Schema::create('votes', function (Blueprint $table) {
             $table->id();
-            $table->string('org_path');
-            $table->string('thumbnail_path')->nullable();
-            $table->text('description')->nullable();
             $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('image_id');
+            $table->integer('value');
 
-            $table->foreign('user_id')->on('users')->references('id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('image_id')->references('id')->on('images');
+
             $table->timestamps();
         });
     }
@@ -32,6 +33,6 @@ class CreateImagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('images');
+        Schema::dropIfExists('votes');
     }
 }
